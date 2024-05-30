45918b9a75b1ea2dad1713aec12446a55474f525
Lumaa <lumaa@lumaa.fr>
2024-05-30 09:16:50 +0200

    progress view on load

--- Artisticly/Views/ContentView.swift
+++ Artisticly/Views/ContentView.swift
@@ -5,34 +5,46 @@ import SwiftData
 
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext: ModelContext
+    
     @State private var browser: MusicBrowser? = nil
+    @State private var fetching: Bool = false
     
     var body: some View {
         ZStack {
-            if let browser = self.browser, browser.setup {
-                SongList(browser: browser)
+            if !fetching {
+                if let browser = self.browser, browser.setup {
+                    SongList(browser: browser)
+                } else {
+                    WelcomeView(browser: $browser)
+                }
             } else {
-                WelcomeView(browser: $browser)
+                ProgressView()
+                    .progressViewStyle(.circular)
             }
         }
         .task {
+            fetching = true
             if let tempBrowser: MusicBrowser = try? await MusicBrowser() {
                 do {
                     let res: CodeResponse = try await tempBrowser.get(path: "/code")
                     
                     if !res.correct {
                         UserDefaults.standard.removeObject(forKey: "code")
+                        fetching = false
                     } else {
                         self.browser = tempBrowser
                         self.browser?.setup = true
+                        fetching = false
                     }
                 } catch {
                     self.browser = nil
+                    fetching = false
                     print(error)
                 }
             } else {
                 UserDefaults.standard.removeObject(forKey: "server")
                 UserDefaults.standard.removeObject(forKey: "code")
+                fetching = false
             }
         }
     }
